df2e90ccab0e9ec08831fc3c2cdba9ead94f4565
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-04-19 21:11:21 +1000

    add md5 hasher

--- hasher.go
+++ hasher.go
@@ -1,9 +1,18 @@
 package main
 
-import "crypto/sha256"
+import (
+	"crypto/md5"
+	"crypto/sha256"
+)
 
 func sha256_bit_hash_from_string(str string) []byte {
 	hash_32 := sha256.Sum256([]byte(str))
 	hash := hash_32[:]
+	return hash
+}
+
+func md5_128_bit_hash_from_string(str string) []byte {
+	hash_16 := md5.Sum([]byte(str))
+	hash := hash_16[:]
 	return hash
 }
